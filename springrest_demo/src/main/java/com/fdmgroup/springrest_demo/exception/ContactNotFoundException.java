94e4b76fe972d7d1fa8d32965920e449e48108b1
dillon.coleman <coleman399@gmail.com>
2022-05-09 14:33:17 -0700

    Annotated ContactNotFoundException

--- springrest_demo/src/main/java/com/fdmgroup/springrest_demo/exception/ContactNotFoundException.java
+++ springrest_demo/src/main/java/com/fdmgroup/springrest_demo/exception/ContactNotFoundException.java
@@ -1,5 +1,9 @@
 package com.fdmgroup.springrest_demo.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ContactNotFoundException extends Exception {
     public ContactNotFoundException() {
         super("Contact Not Found!");
